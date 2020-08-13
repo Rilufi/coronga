826b937a22c4859885cb1e383bc0de1606933602
Rilufi <48300685+Rilufi@users.noreply.github.com>
2020-08-13 13:59:30 -0300

    Update corona_bot.py

--- corona_bot.py
+++ corona_bot.py
@@ -9,19 +9,16 @@ from credentials import *
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
-abspath = os.path.abspath(__file__)
-dname = os.path.dirname(abspath)
-os.chdir(dname)
 
-confirmed = str(JHU.Brazil.confirmed)
-deaths = str(JHU.Brazil.deaths)
-recovered = str(JHU.Brazil.recovered)
+confirmed = (JHU.Brazil.confirmed)
+deaths = (JHU.Brazil.deaths)
+recovered = (JHU.Brazil.recovered)
 
 mystring = f"""COVID-19 no Brasil
 
-Casos confirmados: {confirmed}
-Mortes: {deaths}
-Recuperados: {recovered}
+Casos confirmados: {confirmed:,}
+Mortes: {deaths:,}
+Recuperados: {recovered:,}
 
 Fonte: https://systems.jhu.edu/research/public-health/ncov/"""
 
